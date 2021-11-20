414a6fc944760e280f51bed315a26227f1b9c2e2
DevBorisElkin <ea.foreverrr@gmail.com>
2021-11-20 16:50:11 +0300

    added and updated message counter, but didn't implement

--- EntryPoint/Program.cs
+++ EntryPoint/Program.cs
@@ -185,6 +185,8 @@ namespace EntryPoint
                     }
                     else if (message.StartsWith(CLIENT_SHARES_PLAYROOM_POSITION))
                     {
+                        awd
+
                         string[] substrings = message.Split("|");
                         string[] positions = substrings[1].Split("/");
                         Vector3 position = new Vector3(
--- Server/Util_Server.cs
+++ Server/Util_Server.cs
@@ -365,5 +365,55 @@ namespace ServerCore
         }
 
         #endregion
+
+        #region Message Counter
+
+        public static class MessageCounter
+        {
+            static List<MessageCounterInstance> counterInstances = new List<MessageCounterInstance>();
+            public static void StartCounting(string key)
+            {
+                MessageCounterInstance counterInstance = new MessageCounterInstance(key);
+                counterInstances.Add(counterInstance);
+            }
+
+            public static void UpdateCounter(string key)
+            {
+                MessageCounterInstance correctInstance = GetInstanceByKey(key);
+                if(correctInstance != null)
+                {
+                    correctInstance.counter++;
+                    if ((DateTime.Now - correctInstance.countInceptionDate).TotalMilliseconds > 1000)
+                    {
+                        Console.WriteLine($"[{key}] Messages count: {correctInstance.counter}");
+                        correctInstance.countInceptionDate = DateTime.Now;
+                        correctInstance.counter = 0;
+                    }
+                }
+                else Console.WriteLine($"Error, counter instance for key {key} is null");
+            }
+
+            static MessageCounterInstance GetInstanceByKey(string key)
+            {
+                foreach (var a in counterInstances)
+                    if (a.key == key) return a;
+                return null;
+            }
+
+            class MessageCounterInstance
+            {
+                public string key;
+                public DateTime countInceptionDate;
+                public int counter;
+
+                public MessageCounterInstance(string key)
+                {
+                    this.key = key;
+                    countInceptionDate = DateTime.Now;
+                    counter = 0;
+                }
+            }
+        }
+        #endregion
     }
 }
